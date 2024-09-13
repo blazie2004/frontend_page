01b0da3a21d67f537013f67199c40d8e4c4fd0c6
jay <jayvc1703@gmail.com>
2024-09-14 04:44:14 +0530

    made_navebar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './Navbar.css';
+import logo from '../../assets/Logo.png'; // Correctly import the image
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function Navbar() {
+    return (
+        <div className="navbar-container">
+            <div className="Logo-wrapper">
+                <img src={logo} alt="Logo" />
+            </div>
+            <div className="search-wrapper">
+                <div className="input-search">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} style={{ marginRight: '10px' }} />
+                    <input
+                        className="form-control form-control-sm shadow-none"
+                        type="text"
+                        placeholder="Search Trademark Here e.g., Mickey Mouse"
+                        style={{ border: 'none' }} // Remove border
+                    />
+                </div>
+                <button className="button">Search</button>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
